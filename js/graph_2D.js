e4d9f54fadd010f0c9d954cda5d09fde1f063edf
a-distante1999 <adistante1999@gmail.com>
2022-12-28 17:20:50 +0100

    update

--- js/graph_2D.js
+++ js/graph_2D.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     // set the dimensions and margins of the graph
     const margin = { top: 10, right: 20, bottom: 30, left: 50 },
-        width = 1500 - margin.left - margin.right,
+        width = 1460 - margin.left - margin.right,
         height = 450 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
