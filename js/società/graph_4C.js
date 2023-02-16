fe3619d7df7512462430a1520bfb6fa2c2aa787d
hannademaria <demariahanna@libero.it>
2023-02-16 14:27:20 +0100

    update

--- js/società/graph_4C.js
+++ js/società/graph_4C.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     function draw() {
         let clientHeight = document.getElementById('graph_4C').clientHeight - 100;
         let clientWidth = document.getElementById('graph_4C').clientWidth - 100;
-        console.log(clientHeight)
+
         // set the dimensions and margins of the graph
         const margin = { top: 30, right: 20, bottom: 40, left: 65 };
         if (aux == 1) {
@@ -18,72 +18,76 @@ $(document).ready(function () {
         }
         aux = 1;
 
-
         // append the svg object to the body of the page
         const svg = d3.select("#graph_4C")
             .append("svg")
             .attr("width", clientWidth + margin.left + margin.right)
             .attr("height", clientHeight + margin.top + margin.bottom)
             .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top})`);
+            .attr("transform", `translate(${margin.left},${margin.top})`)
 
-        // Parse the Data
-        d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_stacked.csv").then(function (data) {
+        // Inizitialization array all data links and aux variable
+        let dataValue = [];
+        let year = ["2019", "2020", "2021"];
+        let state = ["DE", "ES", "FR", "IT"];
+        let age =["u18", "18-24", "25-49", "50-64", "65-74", "o75"];
+        
 
-            // List of subgroups = header of the csv files = soil condition here
-            const subgroups = data.columns.slice(1)
+            // Parse the Data
+            // d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_stacked.csv")
+            d3.csv("../../csv/società/graph_4C.csv").then(function (data) {
+                console.log(data)
 
+                // List of subgroups = header of the csv files = soil condition here
+                const subgroups = data.columns.slice(1)
 
-            // List of groups = species here = value of the first column called group -> I show them on the X axis
-            const groups = data.map(d => (d.group))
+                // List of groups = species here = value of the first column called group -> I show them on the X axis
+                const groups = data.map(d => (d.group))
 
+                // Add X axis
+                const x = d3.scaleLinear()
+                    .domain([60, 0])
+                    .range([clientWidth, 0])
+                svg.append("g")
+                    .attr("transform", `translate(0, ${clientHeight})`)
+                    .call(d3.axisBottom(x).tickSizeOuter(0));
 
-            // Add X axis
-            const x = d3.scaleLinear()
-                .domain([60, 0])
-                .range([clientWidth, 0])
-            svg.append("g")
-                .attr("transform", `translate(0, ${clientHeight})`)
-                .call(d3.axisBottom(x).tickSizeOuter(0));
+                // Add Y axis
+                const y = d3.scaleBand()
+                    .domain(groups)
+                    .range([0, clientHeight])
+                    .padding([0.2])
 
+                svg.append("g")
+                    .call(d3.axisLeft(y));
 
-            // Add Y axis
-            const y = d3.scaleBand()
-                .domain(groups)
-                .range([0, clientHeight])
-                .padding([0.2])
+                // color palette = one color per subgroup
+                const color = d3.scaleOrdinal()
+                    .domain(subgroups)
+                    .range(['#e41a1c', '#377eb8', '#4daf4a'])
 
-            svg.append("g")
-                .call(d3.axisLeft(y));
+                //stack the data? --> stack per subgroup
+                const stackedData = d3.stack()
+                    .keys(subgroups)
+                    (data)
 
-            // color palette = one color per subgroup
-            const color = d3.scaleOrdinal()
-                .domain(subgroups)
-                .range(['#e41a1c', '#377eb8', '#4daf4a'])
+                // Show the bars
+                svg.append("g")
+                    .selectAll("g")
+                    // Enter in the stack data = loop key per key = group per group
+                    .data(stackedData)
+                    .join("g")
+                    .attr("fill", d => color(d.key))
+                    .selectAll("rect")
+                    // enter a second time = loop subgroup per subgroup to add all rectangles
+                    .data(d => d)
+                    .join("rect")
+                    .attr("x", d => x(d[0]))
+                    .attr("y", d => y(d.data.group))
+                    .attr("width", d => x(d[1]) - x(d[0]))
+                    .attr("height", y.bandwidth())
 
-            //stack the data? --> stack per subgroup
-            const stackedData = d3.stack()
-                .keys(subgroups)
-                (data)
-
-            // Show the bars
-            svg.append("g")
-                .selectAll("g")
-                // Enter in the stack data = loop key per key = group per group
-                .data(stackedData)
-                .join("g")
-                .attr("fill", d => color(d.key))
-                .selectAll("rect")
-                // enter a second time = loop subgroup per subgroup to add all rectangles
-                .data(d => d)
-                .join("rect")
-                .attr("x", d => x(d[0]))
-                .attr("y", d => y(d.data.group))
-                .attr("width", d => x(d[1]) - x(d[0]))
-                .attr("height", y.bandwidth())
-
-
-        })
+            })
     }
 
 })
